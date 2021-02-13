1a5d8b2a00cbadac62baf4e947e00d83c4272ab1
jmcveigh15 <jmcveigh15@qub.ac.uk>
2021-02-13 15:30:57 +0000

    Add DoublyLinkedLists.java and update EmployeeNode and EmployeeLinkedList with methods for added class

--- src/lists/EmployeeLinkedList.java
+++ src/lists/EmployeeLinkedList.java
@@ -2,12 +2,34 @@ package src.lists;
 
 public class EmployeeLinkedList {
     private EmployeeNode head;
+    private EmployeeNode tail;
     private int size;
 
     public void addToFront(Employee employee) {
         EmployeeNode node = new EmployeeNode(employee);
         node.setNext(head);
+
+        if (head == null) {
+            tail = node;
+        } else {
+            head.setPrevious(node);
+        }
+
         head = node;
+        size++;
+    }
+
+    public void addToEnd(Employee employee) {
+        EmployeeNode node = new EmployeeNode(employee);
+
+        if (tail == null) {
+            head = node;
+        } else {
+            tail.setNext(node);
+            node.setPrevious(tail);
+        }
+
+        tail = node;
         size++;
     }
 
@@ -20,14 +42,42 @@ public class EmployeeLinkedList {
     }
 
     public EmployeeNode removeFromFront() {
+
         if (isEmpty()) {
             return  null;
         }
 
         EmployeeNode removedNode = head;
+
+        if (head.getNext() == null) {
+            tail = null;
+        } else {
+            head.getNext().setPrevious(null);
+        }
+
         head = head.getNext();
         size--;
         removedNode.setNext(null);
+        return removedNode;
+    }
+
+    public EmployeeNode removeFromEnd() {
+
+        if (isEmpty()) {
+            return  null;
+        }
+
+        EmployeeNode removedNode = tail;
+
+        if (tail.getPrevious() == null) {
+            head = null;
+        } else {
+            tail.getPrevious().setNext(null);
+        }
+
+        tail = tail.getPrevious();
+        size--;
+        removedNode.setPrevious(null);
         return removedNode;
     }
 
@@ -36,7 +86,7 @@ public class EmployeeLinkedList {
         System.out.print("HEAD -> ");
         while (current != null) {
             System.out.print(current);
-            System.out.print(" -> ");
+            System.out.print(" <=> ");
             current = current.getNext();
         }
         System.out.println("null");
