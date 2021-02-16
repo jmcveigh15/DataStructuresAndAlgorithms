a13ef9de34516de82776867b1b9af263f7785302
jmcveigh15 <jmcveigh15@qub.ac.uk>
2021-02-16 22:49:39 +0000

    Add classes for stack using an array

---
+++ src/stacks/StackWithArray.java
@@ -0,0 +1,19 @@
+package src.stacks;
+
+public class StackWithArray {
+
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(10);
+
+        stack.push(new Employee("Jane", "Jones", 123));
+        stack.push(new Employee("John", "Doe", 4567));
+        stack.push(new Employee("Mary", "Smith", 22));
+        stack.push(new Employee("Mike", "Wilson", 3245));
+        stack.push(new Employee("Bill", "End", 78));
+
+        stack.printStack();
+        System.out.println("Popped " + stack.pop());
+        System.out.println(stack.peek());
+    }
+
+}
